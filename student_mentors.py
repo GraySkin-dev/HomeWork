dedaafafadeab6e231ccbc7f1304607658d3b348
Sergey Yolkin <sergeyyolkinmail@gmail.com>
2024-12-08 10:56:26 +0300

    Issue #1

--- student_mentors.py
+++ student_mentors.py
@@ -6,13 +6,15 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+ 
+ 
 class Mentor:
+ 
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+ 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,6 +23,19 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+ 
+ 
+class Lecturer(Mentor):
+ 
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+ 
+ 
+class Reviewer(Mentor):
+ 
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+ 
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
